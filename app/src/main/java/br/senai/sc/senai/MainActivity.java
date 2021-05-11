ee7e1d2a51c946feaa24d2a5d5059fca3a79c855
Rodolfo Schmitt Stahelin <67396757+rodolostahelin@users.noreply.github.com>
2021-05-10 22:23:39 -0300

    commit inicial da primeira Activity

---
+++ app/src/main/java/br/senai/sc/senai/MainActivity.java
@@ -0,0 +1,34 @@
+package br.senai.sc.senai;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+    public void onClickBtnMostrar(View v) {
+        EditText editText = findViewById(R.id.et_texto);
+        EditText editText1 = findViewById(R.id.et_texto2);
+        Toast.makeText(MainActivity.this, editText.getText().toString() + editText1.getText(), Toast.LENGTH_LONG).show();
+    }
+    
+    public void onClickBtnLimpar(View v) {
+        EditText editText = findViewById(R.id.et_texto);
+        EditText editText1 = findViewById(R.id.et_texto2);
+
+        editText.setText("");
+        editText1.setText("");
+    }
+
+}
